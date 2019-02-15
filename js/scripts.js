dfec0b4a4186186b19cbe4871462a008f8d4bb26
Matthias Rudin <matthias.rudin@live.com>
2019-02-15 21:01:40 +0100

    Add extractInformation function

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,16 @@ Fetch function
 fetch(url)
     .then(response => response.json())
     .then(data => users.push(...data.results));
+
+/************************************************************************************
+Utility function
+************************************************************************************/
+const extractInformation = object => (
+    {
+        picture: object.picture.thumbnail,
+        firstName: object.name.first,
+        lastName: object.name.last,
+        email: object.email,
+        location: object.location.city
+    }
+)
