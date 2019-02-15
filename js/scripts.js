a13efe68140e05c9c4657caa20114f60da43bbc3
Matthias Rudin <matthias.rudin@live.com>
2019-02-15 21:06:52 +0100

    Add formatCard function

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,20 @@ const extractInformation = object => (
         firstName: object.name.first,
         lastName: object.name.last,
         email: object.email,
-        location: object.location.city
+        city: object.location.city,
+        state: object.location.state,
     }
 )
+
+const formatCard = object => (`
+    <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src=${object.picture} alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${object.firstName} ${object.lastName}</h3>
+            <p class="card-text">${object.email}</p>
+            <p class="card-text cap">${object.city}, ${object.state}</p>
+        </div>
+    </div>
+`)
