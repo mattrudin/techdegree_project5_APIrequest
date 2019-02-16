c392a6e1350b1398dc12a8eabb6220b9ab55b51d
Matthias Rudin <matthias.rudin@live.com>
2019-02-16 15:32:03 +0100

    Add formatModal function

--- js/scripts.js
+++ js/scripts.js
@@ -21,8 +21,13 @@ const extractInformation = object => (
         firstName: object.name.first,
         lastName: object.name.last,
         email: object.email,
+        cellphone: object.cell,
+        birthday: object.dob.date,
         city: object.location.city,
         state: object.location.state,
+        postcode: object.location.postcode,
+        street: object.location.street,
+        nation: object.nat,
     }
 )
 
@@ -35,6 +40,24 @@ const formatCard = object => (`
             <h3 id="name" class="card-name cap">${object.firstName} ${object.lastName}</h3>
             <p class="card-text">${object.email}</p>
             <p class="card-text cap">${object.city}, ${object.state}</p>
+        </div>
+    </div>
+`)
+
+const formatModal = object => (`
+    <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src=${object.picture} alt="profile picture">
+                <h3 id="name" class="modal-name cap">${object.firstName} ${object.lastName}</h3>
+                <p class="modal-text">${object.email}</p>
+                <p class="modal-text cap">${object.city}</p>
+                <hr>
+                <p class="modal-text">${object.cellphone}</p>
+                <p class="modal-text">${object.street}, ${object.state}, ${object.nation} ${object.postcode}</p>
+                <p class="modal-text">Birthday: ${new Date(Date.parse(object.birthday)).toLocaleDateString("en-US")}</p>
+            </div>
         </div>
     </div>
 `)
